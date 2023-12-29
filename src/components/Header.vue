2c482d5f746d15ef133be37617d441f11bd0b1d3
LuizBlume <luiz.fernando.251107@gmail.com>
2023-12-29 00:27:38 -0300

    hotfix: Header is init

---
+++ src/components/Header.vue
@@ -0,0 +1,72 @@
+<template>
+  <header>
+    <nav class="navbar">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img
+            src="../assets/images/logo.png"
+            alt="Logo"
+            width="150"
+            height="150"
+            class="d-inline-block align-text-top"
+          />
+        </a>
+        <ul class="nav justify-content-end">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link">
+              <img src="../assets/images/favoritos.png" alt="" class="icons" />
+              <div class="text-icons">
+                Favoritos
+              </div>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/login" class="nav-link">
+              <img src="../assets/images/cesta.png" alt="" class="icons" />
+              <div class="text-icons">
+                Cesta
+              </div>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/login" class="nav-link">
+              <img src="../assets/images/perfil.png" alt="" class="icons" />
+              <div class="text-icons">
+                Perfil
+              </div>
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
+<style scoped>
+header {
+  padding: 20px 5px;
+  background-color: #5687a6;
+}
+a {
+  color: #fff !important;
+  font-size: 2rem;
+}
+.container-fluid {
+  --bs-gutter-x: 0;
+}
+.icons {
+    width: 70px;
+    height: 70px;
+    margin: 0 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+.text-icons {
+    font-size: 1.7rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+</style>
